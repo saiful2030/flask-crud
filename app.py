029adfa7248c4d6c7d9c2e87bc18420b660f1a08
saifulaji20 <73286300+saiful2030@users.noreply.github.com>
2024-12-11 14:08:17 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import render_template
+from mysql import connector
+from flask import request,redirect, url_for
+
+
+app =  Flask(__name__)
+
+db = connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '',
+    database = 'db_kuliah01'
+)
+if db.is_connected():
+    print('open connection successful')
+    
+
+
+@app.route('/')
+def halaman_awal():
+    cursor = db.cursor()
+    cursor.execute('select * from mahasiswa')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('index.html', hasil = result)
+
+@app.route('/tambah/')
+def tambah_data():
+    return render_template('tambah.html')
+
+@app.route('/proses_tambah/', methods=['POST'])
+def proses_tambah():
+    nim = request.form['nim']
+    nama = request.form['nama']
+    asal = request.form['asal']
+    cur = db.cursor()
+    cur.execute('INSERT INTO mahasiswa (nim,nama,asal) VALUES (%s,%s,%s)',(nim,nama,asal))
+    db.commit()
+    return redirect(url_for('halaman_awal'))
+
+@app.route('/ubah/<nim>', methods=['GET'])
+def ubah_data(nim):
+    cur = db.cursor()
+    cur.execute('select * from mahasiswa where nim=%s', (nim,))
+    res = cur.fetchall()
+    cur.close()
+    return render_template('ubah.html', hasil=res)
+
+@app.route('/proses_ubah/', methods=['POST'])
+def proses_ubah():
+    no_mhs = request.form['nim_ori']
+    nim = request.form['nim']
+    nama = request.form['nama']
+    asal = request.form['asal']
+    cur = db.cursor()
+    sql = "UPDATE mahasiswa SET nim=%s, nama=%s, asal=%s WHERE nim=%s"
+    value = (nim,nama,asal,no_mhs)
+    cur.execute(sql, value)
+    db.commit()
+    return redirect(url_for('halaman_awal'))
+
+@app.route('/hapus/<nim>', methods=['GET'])
+def hapus_data(nim):
+    cur = db.cursor()
+    cur.execute('DELETE from mahasiswa where nim=%s', (nim,))
+    db.commit()
+    return redirect(url_for('halaman_awal'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db_kuliah01.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Des 2023 pada 01.45
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kuliah01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `asal` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `asal`) VALUES
+('22.83.0789', 'muhamamd saiful aji', 'madiun'),
+('22.83.0794', 'alfiansyah naufal', 'klaten'),
+('22.83.0786', 'muh fiki saefulloh', 'kebumen'),
+('22.83.0803', 'ridho fathoni moqorrobin', 'klaten'),
+('22.83.0799', 'putri rahmawati', 'riau');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
